51328c1291a73a5de012edd598fd8e7bfbe87c95
warda-zakria <165257623+warda-zakria@users.noreply.github.com>
2024-03-30 00:48:34 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+//Asking Questions from Users through Inquirer
+
+let answers=await inquirer.prompt([
+    {message:"Enter First Number", type: "number", name:"firstNumber"},
+    {message:"Enter Second Number", type:"number", name:"secondNumber"},
+    {
+        message:"Select one Operator to perfrom operations",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subtractions", "Multiplication", "Division"],
+    },
+]);
+
+// Conditional statements If-Else
+
+if(answers.operator === "Addition"){
+    console.log(answers.firstNumber + answers.secondNumber)
+}
+else if (answers.operator === "Subtractions"){
+    console.log(answers.firstNumber - answers.secondNumber)
+}
+else if (answers.operator === "multiplication"){
+    console.log(answers.firstNumber * answers.secondNumber)
+}
+else if (answers.operator === "Division"){
+    console.log(answers.firstNumber / answers.secondNumber)
+}
+else{
+    console.log("Invalid Input")
+}
